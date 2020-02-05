8c72d9f8614d8bbddd06e5c7897e6d2e07f97688
Dan Sarich <dsarich33@gmail.com>
2020-02-04 22:16:08 -0500

    Updated styles for grid titles / New logic for recient searches view

--- weather/app/static/js/main.js
+++ weather/app/static/js/main.js
@@ -2,48 +2,63 @@
  * Google Charts: Line / Multi Line
  * Vue.js Build Templates for each type of table. 
  * - Pass in X & Y Axis labels and value arrays
+ * 
+ * https://bootstrapious.com/p/circular-progress-bar
 ************************************************/
 var init = function () {
 	google.charts.load('current', { packages: [ 'line' ] });
 
 	var ac = new google.maps.places.Autocomplete(document.getElementById('locationSearch'), {
 		types  : [ 'geocode' ],
-		fields : [ 'formatted_address', 'geometry.location' ]
+		fields : [ 'formatted_address', 'geometry.location', 'icon', 'photos', 'name' ]
 	});
+
 	loadRecients();
+
 	google.maps.event.addListener(ac, 'place_changed', function () {
 		console.log(ac);
 		var place = ac.getPlace();
 		var lat = place.geometry.location.lat();
 		var lng = place.geometry.location.lng();
-		console.log(lat, lng, place);
-		getChartData(lat, lng);
-		saveToRecients(lat, lng, place.formatted_address);
+
+		getChartData(lat, lng, place.name);
 	});
 };
 
-var getChartData = function (lat_in, lng_in) {
+var getChartData = function (lat_in, lng_in, name) {
 	$.ajax({
 		type        : 'POST',
 		contentType : 'application/json',
 		data        : JSON.stringify({ lat: lat_in, lng: lng_in }),
 		url         : '/data',
 		success     : function (result) {
+			createTitleArea(name);
 			formatChartData(result);
+			saveToRecients(lat_in, lng_in, name);
 		},
 		error       : function (err) {
 			console.error(err);
 		}
 	});
 };
-
+var createTitleArea = function (name) {
+	$('#titleContainer').empty();
+	var col = $('<div>', { class: 'col-12 ' }).prependTo('#titleContainer');
+	var heading = $('<h1>', { text: name, class: 'text-white' }).prependTo(col);
+};
 var formatChartData = function (chartData) {
 	$('#chartConatiner').empty();
-
 	for (key in chartData.chart_pairing) {
 		chartData.chart_pairing[key].labels.forEach(function (time, i) {
 			chartData.chart_pairing[key].labels[i] = new Date(time);
 		});
+
+		chartData.chart_pairing[key].rows.forEach(function (rows, i) {
+			rows.forEach(function (row, j) {
+				rows[j] = row.toFixed(2);
+			});
+		});
+
 		createContainer(key, chartData.chart_pairing[key].fullScreen);
 		drawChart(chartData.chart_pairing[key], key);
 	}
@@ -56,7 +71,7 @@ var createContainer = function (chart_id, fs) {
 		var col = $('<div>', { class: 'col-lg-12 col-xl-6 mt-3' }).appendTo('#chartConatiner');
 	}
 
-	var card = $('<div>', { class: 'card ' }).appendTo(col);
+	var card = $('<div>', { class: 'card cardTheme-dark' }).appendTo(col);
 	var card_body = $('<div>', { class: 'card-body' }).appendTo(card);
 	var canvas = $('<canvas>', { id: chart_id }).appendTo(card_body);
 };
@@ -64,61 +79,9 @@ var createContainer = function (chart_id, fs) {
 var drawChart = function (chartData, container) {
 	var myChartData = [];
 	var chartDiv = document.getElementById(container).getContext('2d');
-	// var data = new google.visualization.DataTable();
-
-	// chartData.labels.forEach(function (label, i) {
-	// 	var colType = i == 0 ? 'datetime' : 'number';
-	// 	data.addColumn(colType, label);
-	// });
-
-	// chartData.rows.forEach(function (rows, i) {
-	// 	rows.forEach(function (innerRow, j) {
-	// 		if (i == 0) {
-	// 			myChartData.push([]);
-	// 			myChartData[j].push(new Date(innerRow));
-	// 		} else {
-	// 			myChartData[j].push(innerRow);
-	// 		}
-	// 	});
-	// });
-	// data.addRows(myChartData);
-	// var chartOpts = {
-	// 	chart     : {
-	// 		title : chartData.title
-	// 	},
-	// 	legend    : { position: chartData.show_legend },
-	// 	hAxis     : {
-	// 		title : chartData.axis_labels.hAxis
-	// 	},
-	// 	vAxis     : {
-	// 		title : chartData.axis_labels.vAxis
-	// 	},
-	// 	width     : '100%',
-	// 	height    : 500,
-	// 	pointSize : 30
-	// };
-	// if (chartData.format == 'percent') {
-	// 	chartOpts.vAxis.format = "#'%'";
-	// 	chartOpts.vAxis.minValue = 0;
-	// 	chartOpts.vAxis.maxValue = 100;
-	// }
-	// var chart = new google.charts.Line(chartDiv);
-	// chart.draw(data, google.charts.Line.convertOptions(chartOpts));
 	var colorChoice = {
-		backgroundColor : [
-			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
-			'rgba(255, 206, 86, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(153, 102, 255, 0.2)',
-			'rgba(255, 159, 64, 0.2)'
-		],
-		borderColor     : [
-			"#247BA0",
-			"#FF1654",
-			"#70C1B3",
-			"#B2DBBF"
-		]
+		borderColor     : [ '#EA6A47', '#A5D8DD', '#1599D8', '#D9DFE2' ],
+		backgroundColor : [ '#EA6A4740', '#A5D8D40D', '#1599D840', '#D9DFE240' ]
 	};
 	var chartOpts = {
 		type    : 'line',
@@ -130,26 +93,38 @@ var drawChart = function (chartData, container) {
 			responsive : true,
 			title      : {
 				display   : true,
-				text      : chartData.title
+				text      : chartData.title,
+				fontSize  : 24,
+				fontColor : '#fff'
+			},
+			legend     : {
+				labels : {
+					fontColor : '#fff'
+				}
 			},
 			scales     : {
 				xAxes : [
 					{
-						type      : 'time',
-						time      : {
-							unit: 'day',
-							displayFormats: {
-								second: 'h:MM:SS',
-								minute: 'h:MM',
-								hour: 'hA',
-								day: 'MMM D',
-								month: 'YYYY MMM',
-								year: 'YYYY',
+						type       : 'time',
+						time       : {
+							unit           : 'day',
+							tooltipFormat  : 'MMM DD h:MM A',
+							displayFormats : {
+								second : 'h:MM:SS',
+								minute : 'h:MM',
+								hour   : 'hA',
+								day    : 'MMM D',
+								month  : 'YYYY MMM',
+								year   : 'YYYY'
 							}
 						},
-						scaleLabel: {
-							display: true,
-							labelString: 'value'
+						scaleLabel : {
+							display     : true,
+							labelString : 'value',
+							fontColor   : '#fff'
+						},
+						ticks      : {
+							fontColor : '#fff'
 						}
 					}
 				],
@@ -157,10 +132,12 @@ var drawChart = function (chartData, container) {
 					{
 						scaleLabel : {
 							display     : true,
-							labelString : chartData.axis_labels.yAxis
+							labelString : chartData.axis_labels.yAxis,
+							fontColor   : '#fff'
 						},
-						ticks: {
-							beginAtZero: true
+						ticks      : {
+							beginAtZero : true,
+							fontColor   : '#fff'
 						}
 					}
 				]
@@ -175,48 +152,91 @@ var drawChart = function (chartData, container) {
 			borderWidth     : 3,
 			backgroundColor : 'transparent',
 			borderColor     : [ colorChoice.borderColor[i] ],
-			lineTension		: 0
+			lineTension     : 0
 		};
 
 		chartOpts.data.datasets.push(rowData);
 	});
 
+	if (chartData.format == 'percent') {
+		chartOpts.options.scales.yAxes[0].ticks['min'] = 0;
+		chartOpts.options.scales.yAxes[0].ticks['max'] = 100;
+		chartOpts.options.scales.yAxes[0].ticks['callback'] = function (value) {
+			return value + '%';
+		};
+	}
+
 	var myLineChart = new Chart(chartDiv, chartOpts);
 };
 
-var saveToRecients = function (lat, lon, place) {
-	var myLocations = (localStorage.getItem("climbingLocations") != null) ? JSON.parse(localStorage.getItem("climbingLocations")) : [];
+var saveToRecients = function (lat, lon, name) {
+	var myLocations = localStorage.getItem('climbingLocations') != null ? JSON.parse(localStorage.getItem('climbingLocations')) : [];
+	var locationObj = {
+		lat  : lat,
+		lon  : lon,
+		name : name
+	};
 
-	 var isExists = myLocations.find(function(location, i){
-		if (location.place == place)return true;
-	})
+	var foundObj = myLocations.find(function (loc, i) {
+		if (loc.name == name) return loc;
+	});
+	var isExists = myLocations.indexOf(foundObj);
 
-	console.log(isExists, myLocations);
-
-	if (!isExists){
-		myLocations.push({
-			lat: lat,
-			lon: lon,
-			place: place
-		})
-		localStorage.setItem('climbingLocations', JSON.stringify(myLocations));
+	if (isExists == -1) {
+		myLocations.unshift(locationObj);
+	} else {
+		myLocations.splice(isExists, 1);
+		myLocations.unshift(foundObj);
 	}
+	console.log(myLocations);
+	localStorage.setItem('climbingLocations', JSON.stringify(myLocations));
+	loadRecients();
 };
 
-var loadRecients = function(){
-	var myLocations = (localStorage.getItem("climbingLocations") != null) ? JSON.parse(localStorage.getItem("climbingLocations")) : [];
+var loadRecients = function () {
+	var myLocations = localStorage.getItem('climbingLocations') != null ? JSON.parse(localStorage.getItem('climbingLocations')) : [];
 
-	myLocations.forEach(function(location, i){
-		var fLat = location.lat,
-			fLon = location.lon,
-			fPlace = location.place;
-	
-		var favLink = $("<a>",{"text": fPlace, "data-lat": fLat, "data-lon": fLon}).appendTo("#favoritesConatiner");
-		favLink.on("click", function(){
-			getChartData(fLat, fLon);
-		})
-	})
+	$('#favoritesConatiner').empty();
+	if (myLocations.length) $('.custom-sidebar').show();
+	myLocations.forEach(function (location, i) {
+		// only show 6 recients
+		if (i < 6) {
+			var fLat = location.lat,
+				fLon = location.lon,
+				fName = location.name;
 
+			var favLink = $('<a>', {
+				class      : 'list-group-item list-group-item-action d-flex justify-content-between align-items-center cardTheme-dark',
+				text       : fName,
+				'data-lat' : fLat,
+				'data-lon' : fLon
+			}).appendTo('#favoritesConatiner');
+
+			favLink.on('click', function () {
+				$('#locationSearch').val('');
+				getChartData(fLat, fLon, fName);
+			});
+		}
+	});
+};
+var initProgressView = function () {
+	$('.progress').each(function () {
+		var value = $(this).attr('data-value');
+		var left = $(this).find('.progress-left .progress-bar');
+		var right = $(this).find('.progress-right .progress-bar');
+
+		if (value > 0) {
+			if (value <= 50) {
+				right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)');
+			} else {
+				right.css('transform', 'rotate(180deg)');
+				left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)');
+			}
+		}
+	});
+};
+var percentageToDegrees = function (percentage) {
+	return percentage / 100 * 360;
 };
 
 $(document).ready(init);
