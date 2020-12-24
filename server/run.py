8c2ae5ae420de8d2067d7437d6b6019a2a73d456
Dan Sarich <Dsarich33@gmail.com>
2020-12-24 15:21:32 -0700

    removal of docker / minor python port adjustment

--- server/run.py
+++ server/run.py
@@ -4,5 +4,4 @@ import os
 app.config.from_object("config.secret_keys")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
