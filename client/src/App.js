00b47df970b8d3444caa8cdc237f22be17b008cd
Dan Sarich <Dsarich33@gmail.com>
2021-02-19 18:46:22 -0700

    Tailwind integration / initial layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We're Live!</h1>
-      </header>
+      <Layout />
     </div>
   );
 }
